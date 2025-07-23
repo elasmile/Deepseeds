1cb47d1dff14c28adc3c51605c79a261072aa2cb
smile <tengendaniella5@gmail.com>
2025-07-23 14:34:18 +0100

    i have added the code of calculating area and perimeter of a rectangle with given inputs, and a program to convert from celsius to fahreneit

--- 2.0 software 1.0,/day1/variablepractice.py
+++ 2.0 software 1.0,/day1/variablepractice.py
@@ -1,8 +1,18 @@
-name = input("What is your name?")
-age = input("Enter your age")
-fav_meal = input("What is your favorite meal")
-school = input("Which school do you attend?")
-department = input("What department are you in?")
-bestfriend = input("Enter your bestfriend's name")
+# name = input("What is your name?")
+# age = input("Enter your age")
+# fav_meal = input("What is your favorite meal")
+# school = input("Which school do you attend?")
+# department = input("What department are you in?")
+# bestfriend = input("Enter your bestfriend's name")
 
-print(f"Your name is {name} and you are {age}years old. Your favorite meal is {fav_meal}. You school in {school} in the department of {department} and your bestfriend's name is {bestfriend}")
+# print(f"Your name is {name} and you are {age}years old. Your favorite meal is {fav_meal}. You school in {school} in the department of {department} and your bestfriend's name is {bestfriend}")
+
+# length = 10
+# width = 5
+# area= length * width
+# perimeter = (length + width)* 2
+# print(f"Then area of the rectangle is {area} and the perimeter is {perimeter}")
+celsius = input("Enter the teemperature in celsuis to be converted to fahrenheit")
+celsius = float(celsius)
+fahrenheit = (celsius * (9/5)) + 32
+print(f" the temperature {celsius} in degree celsius is equivalent to {fahrenheit}fahrenheit")
