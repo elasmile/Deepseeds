91dd9b67b57d4fff043273fadad94417616bfd9f
smile <tengendaniella5@gmail.com>
2025-07-23 14:09:42 +0100

    Creating a personalized profile with user input

---
+++ 2.0 software 1.0,/day1/variablepractice.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+age = input("Enter your age")
+fav_meal = input("What is your favorite meal")
+school = input("Which school do you attend?")
+department = input("What department are you in?")
+bestfriend = input("Enter your bestfriend's name")
+
+print(f"Your name is {name} and you are {age}years old. Your favorite meal is {fav_meal}. You school in {school} in the department of {department} and your bestfriend's name is {bestfriend}")
